c89202f16faafd687f875d2d75a28bbe04ed2338
deflucaseng <lucaseng19@gmail.com>
2024-10-14 23:18:10 -0400

    update

--- serverscript.py
+++ serverscript.py
@@ -8,7 +8,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b'Hello, World!')
 
-def run_server(port=443):
+def run_server(port=8000):
     server_address = ('0.0.0.0', port)
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
     print(f"Server running on all interfaces, port {port}")
@@ -16,7 +16,7 @@ def run_server(port=443):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run a simple HTTP server")
-    parser.add_argument('--port', type=int, default=443, help="Port to run the server on")
+    parser.add_argument('--port', type=int, default=8000, help="Port to run the server on")
     args = parser.parse_args()
     
     run_server(args.port)
