146c0dbff9698eb7af1c3e9f197865a1e3b021f4
deflucaseng <lucaseng19@gmail.com>
2024-12-06 00:40:41 -0500

    Modified server code

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Test test test!');
+  
+  // Shut down the server after sending the response
+  server.close(() => {
+    console.log('Server has been shut down.');
+    process.exit(0);
+  });
 });
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
-
-
-
